d5634dff529518e26f70495b1371d469e0c5c411
youzipi <blake_12@qq.com>
2015-03-31 18:34:34 +0800

    格式化输出图书信息到前台
    Signed-off-by: youzipi <blake_12@qq.com>

--- Library.py
+++ Library.py
@@ -1,7 +1,9 @@
+# coding=utf-8
 from flask import Flask
 from flask import render_template, request, url_for, flash, redirect
 from flask_wtf import Form
 from forms import searchForm
+from main.actions import query_by_keyword
 
 app = Flask(__name__)
 
@@ -17,7 +19,9 @@ def query():
     print keyword
     # url = "http://lib2.nuist.edu.cn/opac/search_rss.php?title="+keyword
     url = "http://lib2.nuist.edu.cn/opac/search_rss.php?title="+keyword
-    return redirect(url)
+    result = query_by_keyword(url)
+    return render_template('result.html', result=result)
+    #return "中文".encode('utf-8').decode('gbk')
 
 if __name__ == '__main__':
     app.debug = True
---
+++ main/models.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*- 
+# __author__ = 'youzipi'
+
+
+class Book:
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return self.__doc__
