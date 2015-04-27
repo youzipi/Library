4ae67baf7c661e4c2b2f73e799d2cfab56f857fb
youzipi <blake_12@qq.com>
2015-04-27 22:25:35 +0800

    Signed-off-by: youzipi <blake_12@qq.com>

--- Library.py
+++ Library.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 # 将函数映射到 URLs
 app.add_url_rule('/', view_func=views.index)
 app.add_url_rule('/index/', view_func=views.index)
-app.add_url_rule('/query/', methods=['POST'], view_func=views.query)
+app.add_url_rule('/query/', view_func=views.query)
+# app.add_url_rule('/query/', methods=['POST'], view_func=views.query)
 # app.add_url_rule('/result_temp/', view_func=views.result_temp)
 app.add_url_rule('/result/', view_func=views.result)
 app.secret_key = 'some_secret'
